10d87073edcfb48d1fed07b09c6cbe4a06e17d4c
qband <q7band@gmail.com>
2016-04-03 22:47:24 +0800

    add http_proxy support for cli script

--- html/cli/run.sh
+++ html/cli/run.sh
@@ -3,4 +3,10 @@ set -e
 cd "`dirname "$(which $0)"`"
 source conf/$1
 
-curl -Ls $URL | pup $SELECTOR | json -a $PROPERTIES
+# proxy support
+if [ "${http_proxy}" != "" ]; then
+	CURL='curl --proxy $http_proxy'
+else
+  CURL='curl'
+fi
+CURL -Ls $URL | pup $SELECTOR | json -a $PROPERTIES
